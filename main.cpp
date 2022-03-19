c06b40585eb8bcca0f56ffc32a51e7388303563c
Marek Składaniec <marekskladaniec@gmail.com>
2022-03-19 21:42:02 +0100

    configuring without selected USE_TRIGONOMETRY_DEGREE

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include "Trygonometria/trygonometria.h"
+
+#define _USE_MATH_DEFINES
+
+#include <cmath>
 #include <iostream>
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+#endif
 
 int main()
-{ 
-	std::cout << degreemath::sin(30) << std::endl;
+{
+	double result;
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		result = cos(45.0);
+	#else
+		result = cos(M_PI / 4.0);
+	#endif
+
+	std::cout << result << std::endl;
 	return 0;
 }

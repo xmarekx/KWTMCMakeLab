3905d2d294624a91ef000d38ec64cc08b168180b
Marek Składaniec <marekskladaniec@gmail.com>
2022-03-19 21:28:36 +0100

    adding new folder (Trygonometria) with CMakeLists.txt, trygonometria.h, trygonometria.cpp. modifying main.cpp and main CMakeLists.txt

---
+++ Trygonometria/trygonometria.cpp
@@ -0,0 +1,25 @@
+#include "trygonometria.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+double degreemath::sin(double degree)
+{
+	//zamiana stopni na radiany: pi radianów = 180 stopni
+	return std::sin(degree * (M_PI / 180)); //funkcja biblioteczna z piku <cmath>
+}
+double degreemath::cos(double degree)
+{
+	//zamiana stopni na radiany: pi radianów = 180 stopni
+	return std::sin(degree * (M_PI / 180)); //funkcja biblioteczna z piku <cmath>
+}
+double degreemath::tg(double degree)
+{
+	//zamiana stopni na radiany: pi radianów = 180 stopni
+	return std::sin(degree * (M_PI / 180)); //funkcja biblioteczna z piku <cmath>
+}
+
+double degreemath::ctg(double degree)
+{
+	//zamiana stopni na radiany: pi radianów = 180 stopni
+	return std::sin(degree * (M_PI / 180)); //funkcja biblioteczna z piku <cmath>
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include "Trygonometria/trygonometria.h"
+#include <iostream>
 
 int main()
 { 
-
+	std::cout << degreemath::sin(30) << std::endl;
 	return 0;
 }
